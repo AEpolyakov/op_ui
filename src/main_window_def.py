98a82e8b70099a82751549dd7a48be2cf82166ae
Alexey Polyakov <aypolyakov@mts.ru>
2025-03-08 18:40:37 +0400

    свойства окна вынесены в main_window_def

---
+++ src/main_window_def.py
@@ -0,0 +1,117 @@
+from typing import Any
+
+
+class MainWindowDef(object):
+    kanaly: Any
+    pr9A317M: Any
+    sbrosy: Any
+    nal_R51: Any
+    ots_R36: Any
+    serv_po: Any
+
+    sbros_strok: Any
+    stroki: Any
+    otkaz_ot_CU: Any
+    peresbros: Any
+    inerc_sopr: Any
+    pom_prin_c: Any
+    zapros: Any
+    sbr_zahv: Any
+    razr_zahv: Any
+
+    ubyv_lchm: Any
+    p_avt_D: Any
+    ruchn_Fd: Any
+    avt_smena_izl: Any
+    zahv_ruchn: Any
+    dist_120: Any
+    lchm_komb: Any
+    dist_40: Any
+    obrab_sdc: Any
+    izl_lchm: Any
+    izl_kni: Any
+    avt_rab: Any
+
+    att20: Any
+    karta_mestn: Any
+    avt_pomeh: Any
+    obzor_nereg: Any
+    razr_peresr: Any
+    att60: Any
+    att40: Any
+    pom_prin_o: Any
+
+    kontr_ib3_100: Any
+    korr_ib3_100: Any
+    perezap_ib3_100: Any
+    podg_8: Any
+    podg_3: Any
+    extr_podgot: Any
+    podgot: Any
+    afk_mfrls: Any
+    poisk_neispr: Any
+    vkl_vys: Any
+    vkl_ekv: Any
+    liter_vv: Any
+
+    tochka: Any
+    tire: Any
+    str_left: Any
+    str_right: Any
+    str_down: Any
+    str_up: Any
+    sloy_down: Any
+    sloy_up: Any
+    otmena: Any
+    vvod_inf: Any
+    sbros_inf: Any
+
+    chislo: Any
+
+    prizn_rpn: Any
+    prizn_sou: Any
+    p_avt_tov: Any
+    vkl_tov: Any
+    vkl_tov_b_izl: Any
+    korr_tochn_tov: Any
+
+    acps60: Any
+    acps61: Any
+    acps610: Any
+    acps62: Any
+    acps620: Any
+    acps621: Any
+
+    op0: Any
+    op1: Any
+    op2: Any
+    op3: Any
+    op4: Any
+    op5: Any
+    op6: Any
+    op7: Any
+    op8: Any
+    op9: Any
+    op10: Any
+    op11: Any
+    op12: Any
+    op13: Any
+    op14: Any
+    op15: Any
+    op16: Any
+    op17: Any
+
+    radio_lchm: Any
+    radio_40: Any
+    checkBox_kontrol: Any
+    checkBox_zapros: Any
+    checkBox_sopr: Any
+    checkBox_strobir: Any
+    checkBox_obzor: Any
+
+    oo_delay: Any
+    leds1: Any
+    leds2: Any
+    leds3: Any
+    leds4: Any
+    leds5: Any
